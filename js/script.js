4deba159ec3c332153918d00ca88515108b8f770
Sara-414 <skdykstra79@gmail.com>
2023-08-04 15:41:05 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const epicMix = [
+  "Red Eyes",
+  "Do You Realize?",
+  "Space Song",
+  "Heavy Balloon",
+  "My Favorite Mutiny",
+  "That's Why We Dance",
+  "For Sure",
+  "Never Tear Us Apart",
+  "Johnny and Mary",
+  "Nothing Compares 2 U",
+  "Try a Little Tenderness",
+  "Baby Girl",
+  "I Found Someone"
+];
+
+const mixList = document.querySelector(".mix");
+const button = document.querySelector(".show-list");
+const total = document.querySelector(".total");
+
+button.addEventListener("click", function () {
+  mixList.classList.remove("hide");
+  button.classList.add("hide");
+  mixInfo(epicMix);
+});
+
+total.innerText = `${epicMix.length} great songs!`;
+
+const mixInfo = function (mix) {
+  mix.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`;
+    mixList.append(li);
+  });
+};
